fc55f0949666b6febc34314f2ba207a4f2a03778
Shakil Khan <shakilkhan496@gmail.com>
2023-08-28 15:51:53 +0600

    code updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { RangeDatePicker } from 'react-google-flight-datepicker';
+import 'react-google-flight-datepicker/dist/main.css';
+import { useState } from 'react';
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState({
+    startDate: null,
+    endDate: null,
+  });
+
+  const onDateChange = ({ startDate, endDate }) => {
+    setSelectedDate({
+      startDate: startDate,
+      endDate: endDate,
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Selected date: from{' '}
+        {selectedDate.startDate
+          ? selectedDate.startDate.toDateString()
+          : 'N/A'}{' '}
+        to{' '}
+        {selectedDate.endDate ? selectedDate.endDate.toDateString() : 'N/A'}
+      </h1>
+      <RangeDatePicker
+        startDate={selectedDate.startDate}
+        endDate={selectedDate.endDate}
+        onChange={(startDate, endDate) => onDateChange({ startDate, endDate })}
+        minDate={new Date(1900, 0, 1)}
+        maxDate={new Date(2100, 0, 1)}
+        dateFormat="D"
+        monthFormat="MMM YYYY"
+        startDatePlaceholder="Start Date"
+        endDatePlaceholder="End Date"
+        disabled={false}
+        className="my-own-class-name"
+        startWeekDay="monday"
+      />
     </div>
   );
 }
